a16bc0a08a8773d31c26407e2dd8e4bed52a4135
杨哲 <40693081+YTZ-Developer@users.noreply.github.com>
2019-09-12 22:57:33 +0800

    Add files via upload

---
+++ Fourth_Page.java
@@ -0,0 +1,15 @@
+package com.mycompany;
+import android.app.*;
+import android.os.*;
+import android.view.*;
+
+public class Fourth_Page extends Fragment
+{
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+	{
+		View view=inflater.inflate(R.layout.fourth_page,null);
+		return view;
+	}
+}
